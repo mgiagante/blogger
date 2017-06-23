14b5bebafe718b783d2ba520f480a48f6dde74ba
Mariano Giagante <mariano.giagante@gmail.com>
2017-06-23 02:39:28 -0300

    Implements the process of interpretting the string of ', ' separated tags received from the creation/edition form and using it to link the article to tag objects with those names (creating them first if they do not exist already)

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,23 @@ class Article < ApplicationRecord
   has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings # Allows to access directly from articles to tags
+
+  # An alternative would be to redefine Tag#to_s to show the tag's name.
+  # That way the method below would need to only call tags.join(", ")
+  def tag_list
+    tags.map { |tag| tag.name }.join(", ")
+  end
+
+  def tag_list=(tag_list_string)
+    tag_names = tag_list_string.split(',').map do |tag_name| 
+      tag_name.strip.downcase 
+    end.uniq
+
+    new_or_found_tags = tag_names.map do |tag_name|
+      Tag.find_or_create_by(name: tag_name)
+    end
+
+    self.tags = new_or_found_tags
+  end
+
 end
